b6d8abaa8f29f7572d54e8842ce275e814c45236
Jasonel2 <Jasonel2@uci.edu>
2024-06-09 01:23:59 -0700

    removed unnecessary comments and added comments for readability

--- Minesweeper_Student-master/Minesweeper_Python/src/MyAI.py
+++ Minesweeper_Student-master/Minesweeper_Python/src/MyAI.py
@@ -29,11 +29,9 @@ class MyAI( AI ):
 		self.totalMines = totalMines
 		self.lastX = startX
 		self.lastY = startY
-
 		self.board = [['?' for _ in range(colDimension)] for _ in range(rowDimension)]
 		self.mines_remaining = totalMines
 		self.actionQueue = []
-
 		self.debug = False
 		self.strong_debug = False
 
@@ -138,15 +136,12 @@ class MyAI( AI ):
 		return action
 	
 	def getUncoveredNeighbors(self, row, col):
-		#print("ROW COL", row, col)
 		directions = [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1)]
 		uncovered = {'0','1','2','3','4','5','6','7','8'}
 		neighbors = []
 		for dir_row, dir_col in directions:
 			r, c = row + dir_row, col + dir_col
 			if (0 <= r < self.rowDimension and 0 <= c < self.colDimension) and (self.board[r][c] in uncovered):
-				#print("R,C",r, c)
-				#print("||", c, self.rowDimension-r-1)
 				neighbors.append((c, self.rowDimension-r-1))
 		return neighbors
 
@@ -159,25 +154,14 @@ class MyAI( AI ):
 					if neighbors:
 						V.add((col, self.rowDimension - row - 1))
 						C.update(neighbors)
-				#else:
-				#	print("skipped", self.board[row][col])
 		return list(V), list(C)
 
 	def backtrackAndGuess(self):
 		V, C = self.buildFrontiers()
 		#print(V, C)
+
 		# if there are no covered frontier tiles just do random move
 		if not V:
-			#print("RANDOM1")
-			for row in range(self.rowDimension):
-				for col in range(self.colDimension):
-					if self.board[row][col] == '?':
-						self.lastX, self.lastY = col, self.rowDimension - row - 1
-						return Action(AI.Action.UNCOVER, self.lastX, self.lastY)
-			return Action(AI.Action.LEAVE)
-		# if the frontier has more than 20 points, then just pick random instead of calculating
-		if len(V) > 18:
-			#print("RANDOM1")
 			for row in range(self.rowDimension):
 				for col in range(self.colDimension):
 					if self.board[row][col] == '?':
@@ -185,39 +169,43 @@ class MyAI( AI ):
 						return Action(AI.Action.UNCOVER, self.lastX, self.lastY)
 			return Action(AI.Action.LEAVE)
 
+		# if the frontier has more than 18 points, then just pick random instead of calculating
+		if len(V) > 18:
+			for row in range(self.rowDimension):
+				for col in range(self.colDimension):
+					if self.board[row][col] == '?':
+						self.lastX, self.lastY = col, self.rowDimension - row - 1
+						return Action(AI.Action.UNCOVER, self.lastX, self.lastY)
+			return Action(AI.Action.LEAVE)
+
+		# assignments are basically all the combinations of where it could be a mine
 		possible_assignments = list(product([0, 1], repeat=len(V)))
 		#print("LENGTH of possible: ",len(possible_assignments))
 		
+		# check if is consistent with our board
+		# if it is consistent, then we will put it in list
 		consistent_assignments = []
 		for assignment in possible_assignments:
 			#if assignment == (0,0,0,0,1,0,1,0,1):
 				if self.isConsistent(assignment, V, C):
 					#print(assignment)
 					consistent_assignments.append(assignment)
-		#print(consistent_assignments)
-		#print("LENGTH of consistent: ",len(consistent_assignments))
+
+		# if it is consistent then find the lowest probable tile that will be a mine
 		if consistent_assignments:
-			# 
-			#print(len(V))
 			assignment_sums = [0] * len(V)
-			#print(assignment_sums)
 			for assignment in consistent_assignments:
 				for i, val in enumerate(assignment):
 					assignment_sums[i] += val
-			#print(assignment_sums)
 			min_sum = min(assignment_sums)
 			safe_indices = [i for i, val in enumerate(assignment_sums) if val == min_sum]
-			#
-
-			#safe_tiles = [V[i] for i, val in enumerate(consistent_assignments[0]) if val == 0]
 			safe_tiles = [V[i] for i in safe_indices]
-			#print("SAFE TILES: ", safe_tiles)
 			if safe_tiles:
 				self.lastX, self.lastY = safe_tiles[0]
 				return Action(AI.Action.UNCOVER, self.lastX, self.lastY)
-				
+
+		# if we still haven't uncovered anything then just pick a random tile and move on
 		for row in range(self.rowDimension):
-			#print("RANDOM2")
 			for col in range(self.colDimension):
 				if self.board[row][col] == '?':
 					self.lastX, self.lastY = col, self.rowDimension - row - 1
@@ -226,29 +214,28 @@ class MyAI( AI ):
 
 	# checking if the assignment is consistent with the board's possibilities
 	def isConsistent(self, assignment, V, C):
-		#print(assignment)
+		# copy the board and write the assignment onto the board
 		board_copy = [row[:] for row in self.board]
 		for i, (col, row) in enumerate(V):
 			if assignment[i] == 1:
 				board_copy[self.rowDimension-row-1][col] = 'F'
 			else:
 				board_copy[self.rowDimension-row-1][col] = '?'
-		#print(board_copy)
+		
+		# with the copied board, check the surrounding uncovered tiles
+		# for example if there are no mines near the uncovered '2' tile, then we know that
+		# this assignment is not consistent with the board, therefore won't be checked and will return false
 		for col, row in C:
-			#print("In C", col, row)
 			num_mines = 0
+			# check uncovered tiles
 			for ncol, nrow in self.getNeighborsOfType(self.rowDimension - row - 1, col, '?'):
-				#print(ncol, nrow)
 				if board_copy[self.rowDimension - nrow - 1][ncol] == 'F':
-					#print('ran')
 					num_mines += 1
+			# check flagged tiles
 			for ncol, nrow in self.getNeighborsOfType(self.rowDimension - row - 1, col, 'F'):
 				if board_copy[self.rowDimension - nrow - 1][ncol] == 'F':
 					num_mines += 1
 			if num_mines != int(self.board[self.rowDimension - row - 1][col]):
-				#print("stopped")
-				#print("Actual number of Mines: ", num_mines)
-				#print("num of minds should be: ", int(self.board[self.rowDimension - row - 1][col]))
 				return False
 		return True
 
@@ -325,24 +312,11 @@ class MyAI( AI ):
 		
 		# Fifth, 1-2-2-1 pattern
 
-		# Running the probabilistic model
-		
-		# NOW, THIS JUST MAKES THE FIRST POSSIBLE MOVE
-		# RANDOM MOVE HERE	
-		
 		# If the puzzle still hasn't been completed to this point, we'll start doing some probability and guessing
-		# For now, I'm just choosing any first random tile xd
-		"""for row in range(self.rowDimension):
-			for col in range(self.colDimension):
-				if self.board[row][col] == '?':	
-					if self.debug:
-						print('making a random move...1')
-					self.lastX, self.lastY = col, self.rowDimension-row-1
-					return Action(AI.Action.UNCOVER, self.lastX, self.lastY)"""
-		
 		action = self.backtrackAndGuess()
 		if action:
 			return action
+			
 		return Action(AI.Action.LEAVE)
 		########################################################################
 		#							YOUR CODE ENDS							   #
